2fafb8e24ed4efd72ea6d365ff0ca044e1812878
Deshmukhadi09 <85308607+Deshmukhadi09@users.noreply.github.com>
2022-05-25 02:42:30 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://aditya:Aditya01@cluster0.agzgv.mongodb.net/meandbs?retryWrites=true&w=majority', err => {
+mongoose.connect('mongodb+srv://<pass>:Aditya01@cluster0.agzgv.mongodb.net/meandbs?retryWrites=true&w=majority', err => {
     if(!err){
         console.log('DB Connection Sucessful');
     } else {
